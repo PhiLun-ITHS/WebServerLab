c673566d62a639538226f82fcfe12223e7c12f30
PL <philip.lundin@iths.se>
2021-06-07 16:33:13 +0200

    added parseUrl and more

--- utils/src/main/java/Utils.java
+++ utils/src/main/java/Utils.java
@@ -1,6 +1,37 @@
+
 public class Utils {
 
-    public String message(){
-        return "hej from UTILS";
+    public static String parseUrl(String input) {
+
+        String[] result = input.split(" ");
+        return result[1];
+    }
+
+    public static HTTPType parseHttpRequestType(String input){
+        if (input.startsWith("G")){
+            return HTTPType.GET;
+        } else if (input.startsWith("H")){
+            return HTTPType.HEAD;
+        } else if (input.startsWith("PO")){
+            return HTTPType.POST;
+        } else {
+            throw new RuntimeException("Invalid type");
+        }
+    }
+
+    public Request parseHttpHeader(String input) {
+
+        var request = new Request();
+        request.type = parseHttpRequestType(input);
+        request.url = parseUrl(input);
+        return request;
+    }
+
+    public static boolean handleRequest(Request request) {
+        return switch (request.type) {
+            case GET -> true;
+            case HEAD -> false;
+            case POST -> true;
+        };
     }
 }
