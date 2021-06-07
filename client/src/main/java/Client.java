d7281f978a504df0b36bbd75aee149f41972cb2c
PL <philip.lundin@iths.se>
2021-06-07 16:33:30 +0200

    updated project

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -7,12 +7,11 @@ import java.net.Socket;
 public class Client {
 
     public static void main(String[] args) {
-
         try {
             Socket socket = new Socket("localhost", 5050);
 
             var output = new PrintWriter(socket.getOutputStream());
-            output.println("hej from client");
+            output.println("Hello from client");
             output.println();
 
             output.flush();
--- core/src/main/java/Main.java
+++ core/src/main/java/Main.java
@@ -4,42 +4,74 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
+    public static List<String> billboard = Collections.synchronizedList(new ArrayList<>());
+
     public static void main(String[] args) {
-        openSocket();
-    }
+        ExecutorService executorService = Executors.newCachedThreadPool();
 
-    private static void openSocket() {
         try(ServerSocket serverSocket = new ServerSocket(5050)){
-
             while(true) {
                 Socket client = serverSocket.accept();
-                handleConnection(client);
+                executorService.submit(() -> handleConnection(client));
             }
         } catch (IOException e){
             e.printStackTrace();
         }
     }
 
-    private static void handleConnection(Socket client) throws IOException{
+    private static void handleConnection(Socket client) {
+        try {
+            var inputFromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-        System.out.println(client.getInetAddress());
-        var inputFromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            readRequest(inputFromClient);
+            var outputToClient = new PrintWriter(client.getOutputStream());
+            sendResponse(outputToClient);
+            inputFromClient.close();
+            outputToClient.close();
+            client.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
-        while(true){
+    private static void sendResponse(PrintWriter outputToClient) {
+        //  outputToClient.print("HTTP/1.1 404 Not Found\r\nContent-length: 0\r\n\r\n");
+        synchronized (billboard) {
+            for (String line : billboard) {
+                outputToClient.print(line + "\r\n");
+            }
+        }
+        outputToClient.println("\r\n");
+        outputToClient.flush();
+    }
+
+    private static void readRequest(BufferedReader inputFromClient) throws IOException {
+        List<String> tempList = new ArrayList<>();
+
+        while (true) {
             var line = inputFromClient.readLine();
-            if (line == null || line.isEmpty()){
+            if (line == null || line.isEmpty()) {
                 break;
             }
+            tempList.add(line);
             System.out.println(line);
         }
-        var outputToClient = new PrintWriter(client.getOutputStream());
-        outputToClient.print("HTTP/1.1 404 Not Found\r\nContent-length: 0\r\n\r\n");
-        outputToClient.flush();
-        inputFromClient.close();
-        outputToClient.close();
-        client.close();
+        synchronized (billboard) {
+            billboard.addAll(tempList);
+        }
     }
 }
+
+/*
+System.out.println(client.getInetAddress());
+            System.out.println(Thread.currentThread().getName());
+            Thread.sleep(500);
+ */
